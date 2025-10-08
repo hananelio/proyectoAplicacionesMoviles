5429416af72c9b22fc0cd7d841a6a0424774a8fc
hananelio <elio_uagrmsoc@hotmail.com>
2025-10-08 05:16:45 -0400

    correcion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { IonMenuButton } from "@ionic/angular/standalone";
 import { Images } from '../../services/images';
 import { HttpClientModule } from '@angular/common/http';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,13 +16,20 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule],
   standalone: true,
 })
-export class HeaderComponent  implements OnInit {
+/*export class HeaderComponent  implements OnInit {
+  pageTitle: string = '';
   images: Record<string, any> = {};
 
-  constructor(private imagesService: Images) { }
+  constructor(private imagesService: Images, private router: Router) { }
 
   ngOnInit() {
     this.loadImages();
+    
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      this.setPageTitle(event.urlAfterRedirects);
+    });
   }
 
   async loadImages() {
@@ -36,4 +45,64 @@ export class HeaderComponent  implements OnInit {
       this.images = {};
     }
   }
+
+  setPageTitle(url: string) {
+    switch (url) {
+      case '/inicio':
+        this.pageTitle = 'Inicio';
+        break;
+      case '/menu':
+        this.pageTitle = 'Menú';
+        break;
+      case '/perfil':
+        this.pageTitle = 'Perfil';
+        break;
+      default:
+        this.pageTitle = '';
+    }
+  }
+}*/
+export class HeaderComponent  implements OnInit {
+  pageTitle: string = '';
+  images: Record<string, any> = {};
+
+  constructor(private imagesService: Images, private router: Router) { }
+
+  ngOnInit() {
+    this.loadImages();
+    
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.setPageTitle(event.urlAfterRedirects);
+      });
+  }
+
+  async loadImages() {
+    try {
+      this.images = this.imagesService.getImages() || await this.imagesService.loadImages();
+      Object.keys(this.images).forEach(key => console.log(`Imagen cargada: ${key}`));
+    } catch (err) {
+      console.error('Error cargando images.json', err);
+      this.images = {};
+    }
+  }
+
+  // ✅ Este método debe estar dentro de la clase
+  setPageTitle(url: string) {
+    switch (url) {
+      case '/inicio':
+        this.pageTitle = 'Inicio';
+        break;
+      case '/menu':
+        this.pageTitle = 'Menú';
+        break;
+      case '/perfil':
+        this.pageTitle = 'Perfil';
+        break;
+      default:
+        this.pageTitle = '';
+    }
+  }
 }
+
